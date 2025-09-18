e9f51176ff7556d533e35f8bb7b8849015f02580
devsamuelv <realalloutmusic@gmail.com>
2025-09-18 00:06:55 -0400

    I do not remember

--- main/main.c
+++ main/main.c
@@ -6,14 +6,16 @@
 #include <stdlib.h>
 #include <esp_intr_alloc.h>
 #include <iot_servo.h>
+#include <hal/wdt_hal.h>
 
 // Arbitrary
 #define DUTY_RESOLUTION LEDC_TIMER_13_BIT
 #define LEDC_SPEED LEDC_HIGH_SPEED_MODE
 #define LEDC_CHANNEL LEDC_CHANNEL_0
 
-void main_thread(void *pvParameters)
+void main_thread(void *context)
 {
+  TickType_t xLastWakeTime;
   servo_config_t servo_config = {
       .max_angle = 180,
       .min_width_us = 500,
@@ -32,36 +34,37 @@ void main_thread(void *pvParameters)
   };
 
   ESP_ERROR_CHECK(iot_servo_init(LEDC_LOW_SPEED_MODE, &servo_config));
+  xLastWakeTime = xTaskGetTickCount();
 
   for (;;)
   {
-    ESP_ERROR_CHECK(iot_servo_write_angle(LEDC_LOW_SPEED_MODE, 0, 100.0f));
+    ESP_ERROR_CHECK(iot_servo_write_angle(LEDC_LOW_SPEED_MODE, 0, 75.0f));
+    xTaskDelayUntil(&xLastWakeTime, 10);
+    ESP_ERROR_CHECK(iot_servo_write_angle(LEDC_LOW_SPEED_MODE, 0, 115.0f));
     esp_task_wdt_reset();
+    wdt_hal_feed(context);
     taskYIELD();
   }
 
   vTaskDelete(NULL);
 }
 
-void task_two(void *pvParameters)
+void serial_task(void *serial_data)
 {
-  for (;;)
-  {
-    printf("hello!");
-    esp_task_wdt_reset();
-    vTaskDelay(pdMS_TO_TICKS(100 / portTICK_PERIOD_MS));
-  }
 }
 
 void app_main()
 {
+  wdt_hal_context_t hal_context;
   TaskHandle_t handle = NULL;
 
-  xTaskCreatePinnedToCore(&main_thread, "servo-process", 8192, NULL, tskIDLE_PRIORITY + 1, &handle, APP_CPU_NUM);
+  wdt_hal_init(&hal_context, WDT_MWDT0, 80, true);
+  wdt_hal_enable(&hal_context);
+
+  xTaskCreate(&main_thread, "servo-process", 1024, &hal_context, tskIDLE_PRIORITY + 1, &handle);
 
   if (handle != NULL)
   {
     ESP_ERROR_CHECK(esp_task_wdt_add(handle));
-    printf("Task added to watchdog!");
   }
 }
